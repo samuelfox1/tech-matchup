42fd013b93967d647e3b285ce78b80bace744d6f
Samuel Fox <samueljasonfox@gmail.com>
2021-08-20 16:59:53 -0700

    add Profile to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,17 @@
-const { Tech, Matchup } = require('../models');
+const { Tech, Matchup, Profile } = require('../models');
+const { signToken } = require('../utils/auth');
+
 
 const resolvers = {
   Query: {
+    profiles: async () => {
+      return Profile.find();
+    },
+
+    profile: async (parent, { profileId }) => {
+      return Profile.findOne({ _id: profileId });
+    },
+
     tech: async () => {
       return Tech.find({});
     },
@@ -11,6 +21,53 @@ const resolvers = {
     },
   },
   Mutation: {
+    addProfile: async (parent, { name, email, password }) => {
+      const profile = await Profile.create({ name, email, password });
+      const token = signToken(profile);
+
+      return { token, profile };
+    },
+    login: async (parent, { email, password }) => {
+      const profile = await Profile.findOne({ email });
+
+      if (!profile) {
+        throw new AuthenticationError('No profile with this email found!');
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(profile);
+      return { token, profile };
+    },
+
+    addSkill: async (parent, { profileId, skill }) => {
+      return Profile.findOneAndUpdate(
+        { _id: profileId },
+        {
+          $addToSet: { skills: skill },
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+    },
+    removeProfile: async (parent, { profileId }) => {
+      return Profile.findOneAndDelete({ _id: profileId });
+    },
+    removeSkill: async (parent, { profileId, skill }) => {
+      return Profile.findOneAndUpdate(
+        { _id: profileId },
+        { $pull: { skills: skill } },
+        { new: true }
+      );
+    },
+
+
     createMatchup: async (parent, args) => {
       const matchup = await Matchup.create(args);
       return matchup;
@@ -23,6 +80,9 @@ const resolvers = {
       );
       return vote;
     },
+    createTech: async (parent, args) => {
+      return await Tech.create(args)
+    }
   },
 };
 
