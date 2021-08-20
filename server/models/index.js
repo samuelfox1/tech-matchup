f27081c989aa89c25730cdaaf265296ad62c2e6f
Samuel Fox <samueljasonfox@gmail.com>
2021-08-20 16:59:31 -0700

    add Profile model

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 const Tech = require('./Tech');
 const Matchup = require('./Matchup');
+const Profile = require('./Profile');
 
-module.exports = { Tech, Matchup };
+module.exports = { Tech, Matchup, Profile };
