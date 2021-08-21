f4a9f53f55d46ac0fd2c0d5099a912952a13c410
Samuel Fox <samueljasonfox@gmail.com>
2021-08-20 17:00:09 -0700

    add Profile typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,14 +14,38 @@ const typeDefs = gql`
     tech2_votes: Int
   }
 
+  type Profile {
+    _id: ID!
+    name: String
+    email: String
+    password: String
+    skills: [String]!
+  }
+
+  type Auth {
+    token: ID!
+    profile: Profile
+  }
+
   type Query {
     tech: [Tech]
     matchups(_id: String): [Matchup]
+
+    profiles: [Profile]!
+    profile(profileId: ID!): Profile
   }
 
   type Mutation {
+    addProfile(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    addSkill(profileId: ID!, skill: String!): Profile
+    removeProfile(profileId: ID!): Profile
+    removeSkill(profileId: ID!, skill: String!): Profile
+
     createMatchup(tech1: String!, tech2: String!): Matchup
     createVote(_id: String!, techNum: Int!): Matchup
+    createTech(name: String!): Tech
   }
 `;
 
